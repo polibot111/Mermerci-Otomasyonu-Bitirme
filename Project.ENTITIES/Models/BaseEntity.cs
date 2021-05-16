45fe72f2233c62d962108e6c1beea4d4afbfc0ad
sanane lan <kchnfrkn@gmail.com>
2021-05-16 16:09:02 +0300

    Yeni Calısma 2021

---
+++ Project.ENTITIES/Models/BaseEntity.cs
@@ -0,0 +1,26 @@
+﻿using Project.ENTITIES.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.ENTITIES.Models
+{
+    public abstract class BaseEntity
+    {
+        public int ID { get; set; }
+        public DateTime? CreatedTime { get; set; }
+        public DateTime? ModifiedTime { get; set; }
+
+        public DateTime? DeleteDate { get; set; }
+        public Status Status{ get; set; }
+
+        public BaseEntity()
+        {
+            Status = Status.Insarted;
+            CreatedTime = DateTime.Now;
+        }
+
+    }
+}
